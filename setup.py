03cdf88ee76075d5c0eda69fd511d8687263cb01
Michael Scholtens <48947001+michaelscholtens@users.noreply.github.com>
2021-05-03 15:44:00 -0400

    edits to required dependencies

--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='PyTangle',
       author_email = 'author.mscholtens@gmail.com',
       download_url = 'https://github.com/michaelscholtens/PyTangle',
       description = 'Python wrapper for the Crowdtangle API that returns results as pandas dataframes.',
-      install_requires = ['pandas', 'numpy', 'requests', 'time', 'logging', 'collections', 'datetime']
+      install_requires = ['pandas', 'numpy', 'requests', 'logging', 'collections', 'datetime']
       )
