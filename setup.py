d1515109ff893503470e4a9c562e7d7e00b9cd75
Michael Scholtens <48947001+michaelscholtens@users.noreply.github.com>
2021-05-03 15:15:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from distutils.core import setup
 setup(name='PyTangle',
       version='0.0.1',
       py_modules=['PyTangle'],
+      author = 'Michael Scholtens',
+      author_email = 'author.mscholtens@gmail.com',
+      download_url = 'https://github.com/michaelscholtens/PyTangle',
+      description = 'Python wrapper for the Crowdtangle API that returns results as pandas dataframes.',
+      install_requires = ['pandas', 'numpy', 'requests', 'time', 'logging', 'collections', 'datetime']
       )
